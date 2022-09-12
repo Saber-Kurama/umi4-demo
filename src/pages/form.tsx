b820f719f2a4b3cdaf339bc56a8a536ac18190d5
guochao <463561248@qq.com>
2022-09-12 23:27:10 +0800

    ✨ feat:  了解一下 effect

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,3 +1,42 @@
+import { createForm, onFormInit } from "@formily/core";
+import { useMemo, useState } from "react";
+import { ActionResponse } from "@/components/ActionResponse";
+
 export default function FormPage() {
-  return <div>这是测试 createForm 的界面</div>;
+  // const form = createForm({
+  //   initialValues: {
+  //     say: "hello",
+  //   },
+  // });
+
+  const [response, setResponse] = useState("");
+  const form = useMemo(
+    () =>
+      createForm({
+        effects() {
+          console.log("???>>>>>");
+          onFormInit(() => {
+            console.log("???");
+            // setResponse("表单已初始化");
+          });
+          console.log("??xx");
+        },
+      }),
+    []
+  );
+  form.addEffects("saber", (form) => {
+    onFormInit(() => {
+      console.log("???1111");
+      // setResponse("表单已初始化");
+    });
+  });
+  form.onInit();
+  form.removeEffects("saber");
+  form.onInit();
+  return (
+    <div>
+      这是测试 createForm 的界面
+      <ActionResponse response={response} />
+    </div>
+  );
 }
